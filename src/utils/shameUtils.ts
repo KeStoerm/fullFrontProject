946711787106b93db4fa81c944bb877000ec96e7
Mikn <mikatheiss97@gmail.com>
2021-06-22 15:52:29 +0200

    reverse result list

--- src/utils/shameUtils.ts
+++ src/utils/shameUtils.ts
@@ -1,10 +1,10 @@
 import { Persons, ShameRanking } from "@/types/types";
-import { map, reduce, size, sortBy } from 'lodash';
+import { map, reduce, size, sortBy, reverse } from 'lodash';
 
 export const analyseShamePointResult = (results: Persons): ShameRanking => {
     const countedShames: ShameRanking = map(results, result => ({name: result.name, rank: 0, shamePointAmount: size(result.shamePoints)}))
 
-    const sortedShames: ShameRanking = sortBy(countedShames, ['shamePointAmount']);
+    const sortedShames: ShameRanking = reverse(sortBy(countedShames, ['shamePointAmount']));
     const rankedShames: ShameRanking = map(sortedShames, (result, index) => ({...result, rank: index }));
 
     return rankedShames;
