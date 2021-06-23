b542013ddb0a2bc62f57724aaa30e6f122dc5f92
Mikn <mikatheiss97@gmail.com>
2021-06-23 06:52:09 +0200

    fix origin url

--- src/controller/shameController.ts
+++ src/controller/shameController.ts
@@ -1,9 +1,13 @@
 import { Person } from "@/types/types"
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+
+const getOrigin = (): string => {
+    return window.location.hostname;
+}
 
 export const getShamePointsForMonth = async (): Promise<Array<Person>> => {
     const date = new Date();
-        const url = process.env.VUE_APP_FULL_FRONT_BACKEND_URL + '/' + date.getFullYear() + '/' + (date.getMonth() + 1) ;
+        const url = getOrigin() + ':1336' + '/' + date.getFullYear() + '/' + (date.getMonth() + 1) ;
         const { data } = await axios.get(url, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
